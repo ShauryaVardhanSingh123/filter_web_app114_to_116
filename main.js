66faabedcfeac172cb4b3528306fb4c70be98ccc
ShauryaVardhanSingh123 <84715057+ShauryaVardhanSingh123@users.noreply.github.com>
2022-10-06 14:24:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,8 +10,22 @@ function setup() {
 
 function preload(){}
 
-function draw(){}
+function draw(){
+    image(video,0,0,300,300)
+}
 
 function takeSnapshot(){
     save("You're_a_clownLOL.png")
 }
+
+function modelLoaded() {  
+    console.log('PoseNet Is Initialized');
+}
+
+    function gotPoses(results){
+    if(results.length > 0){
+    console.log(results);
+    console.log("nose x = " + results[0].pose.nose.x);
+    console.log("nose y = " + results[0].pose.nose.y);
+    }
+    }
